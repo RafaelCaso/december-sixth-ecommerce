539052869bc25c70cde2268acbe2e37fc509de80
rafaelcaso <aracaso@outlook.com>
2022-12-06 13:01:35 -0500

    initial commit

---
+++ src/main/java/com/example/services/ReceiptService.java
@@ -0,0 +1,91 @@
+package com.example.services;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.models.Item;
+import com.example.models.Receipt;
+import com.example.models.User;
+import com.example.repository.ItemRepository;
+import com.example.repository.ReceiptRepository;
+import com.example.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+
+@Service
+@AllArgsConstructor(onConstructor=@__(@Autowired))
+public class ReceiptService {
+	
+	private ReceiptRepository rRepo;
+	private UserRepository uRepo;
+	private ItemRepository iRepo;
+	
+	//used is email is passed(main one to use)
+	public Receipt createReceipt(String email, List<Item> items, Double total) {
+		User u = uRepo.getByEmail(email).get();
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		String now = LocalDateTime.now().format(format);
+		Integer amountOfItems = items.size();
+		Receipt rec = new Receipt(u, items, total, now, amountOfItems);
+		return rRepo.save(rec);
+	}
+	
+	//used if userId is passed(Just an extra precaution)
+	public Receipt createReceipt(Integer id, List<Item> items, Integer amountOfItems) {
+		User u = uRepo.findById(id).get();
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		String now = LocalDateTime.now().format(format);
+		List<Item> receiptItems = new ArrayList<>();
+		Double total = 00.00;
+		System.out.println(amountOfItems);
+		
+		for(Item i : items) {
+			Item addItem = iRepo.findById(i.getItemId()).get();
+			total += addItem.getPrice() * amountOfItems;
+			receiptItems.add(addItem);
+		}
+		Integer amountOfItemsOnReceipt = receiptItems.size();
+		Receipt rec = new Receipt(u, receiptItems, total, now, amountOfItemsOnReceipt);
+		return rRepo.save(rec);
+	}
+	
+	public Receipt readReceipt(Integer id) {
+		return rRepo.findById(id).get();
+	}
+	
+	public Receipt updateReceipt(Receipt r) {
+		Receipt updateReceipt = rRepo.findById(r.getReceiptNumer()).get();
+		
+		updateReceipt.setAmountOfItems(r.getAmountOfItems());
+		updateReceipt.setDateTime(r.getDateTime());
+		updateReceipt.setItems(r.getItems());
+		updateReceipt.setTotal(r.getTotal());
+		updateReceipt.setUser(r.getUser());
+		
+		return rRepo.save(updateReceipt);
+	}
+	
+	public String deleteReceipt(Integer id) {
+		rRepo.delete(rRepo.findById(id).get());
+		return "Receipt has been deleted";
+	}
+	
+	public List<Receipt> getReceiptByUser(User u) {
+		List<Receipt> all = rRepo.findAll();
+		List<Receipt> result = new ArrayList<>();
+		for(Receipt r : all) {
+			if(r.getUser().getUserId() == u.getUserId()) {
+				result.add(r);
+			}
+		}
+		
+		return result;
+	}
+
+}
